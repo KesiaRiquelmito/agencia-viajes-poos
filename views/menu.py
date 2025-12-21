32b63b50e2fff7653a8f4fc9f729c1892e96a8c2
kesiariquelmito <kesiariquelmesoto@gmail.com>
2025-12-21 17:05:27 -0300

    feat: integrate PackageController and UserController into menu

--- views/menu.py
+++ views/menu.py
@@ -1,4 +1,6 @@
 from controllers.destination_controller import DestinationController
+from controllers.package_controller import PackageController
+from controllers.user_controller import UserController
 from db.database import Database
 
 
@@ -6,6 +8,8 @@ class Menu:
     def __init__(self):
         self.db = Database()
         self.destination_controller = DestinationController(self.db)
+        self.user_controller = UserController(self.db)
+        self.package_controller = PackageController(self.db)
 
     def start(self):
         self.destination_controller.list_destinations()
